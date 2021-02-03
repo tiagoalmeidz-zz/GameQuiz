3e5e274e62e0c93dea1ef2f4868257b4c7cdd868
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2021-02-03 01:30:48 -0300

    feat: Widget style

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,46 @@ export const QuizContainer = styled.div`
   }
 `
 
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4CAF50;
+  background-color: #1c1814;
+  border-radius: 10px;
+  overflow: hidden;
+`
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`
+
 export default function Home() {
   return (
     <BackgroundImage>
       <QuizContainer>
-        Hello!
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz sobre games!</h1>
+            <p>Lorem ipsum, dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz da galera.</h1>
+            <p>Lorem ipsum, dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
       </QuizContainer>
     </BackgroundImage>
   )
